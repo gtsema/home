c9e5fff995c9a90f10f84bf8bcf10b090e88ded2
tsema <g.tsema@gmail.com>
2020-10-21 12:54:04 +0300

    initial commit

---
+++ src/main/java/ru/example/HelloServlet.java
@@ -0,0 +1,13 @@
+package ru.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
---
+++ startBrowser.bat
@@ -0,0 +1,3 @@
+@echo off
+start /d "C:\Users\grigoriy\AppData\Local\Google\Chrome\Application\" chrome.exe -incognito http://localhost:8888
+pause

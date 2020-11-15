64e129c560b395824d16d13994cc9eae5d7f4116
gtsema <g.tsema@gmail.com>
2020-11-16 02:08:43 +0300

    tune pipeline

--- jenkinsfiles/deploy.sh
+++ jenkinsfiles/deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo TOMCAT_USER
-echo TOMCAT_PASSWORD
-echo WAR_PATH
-echo TOMCAT_HOST
-echo TOMCAT_PORT
-echo CONTEX_NAME
-
 #URL_RESPONSE=(curl -v -u $TOMCAT_USER:$TOMCAT_PASSWORD -T $WAR_PATH \"http://$TOMCAT_HOST:$TOMCAT_PORT/manager/text/deploy?path=/$CONTEX_NAME&update=true\")
 #
 #if [[ $CURL_RESPONSE == *\"FAIL\"* ]]; then

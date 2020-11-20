db83b9634e742783193e2d16b8b278b98f8d2461
gtsema <42067282+gtsema@users.noreply.github.com>
2020-11-20 19:13:27 +0300

    tune deploy.sh

--- jenkinsfiles/deploy.sh
+++ jenkinsfiles/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo \"http://$TOMCAT_HOST:$TOMCAT_PORT/manager/text/deploy?path=/$CONTEX_NAME&update=true\"
+CURL_RESPONSE=$(curl -u $TOMCAT_USER:$TOMCAT_PASSWORD -T $WAR_PATH http://$TOMCAT_HOST:$TOMCAT_PORT/manager/text/deploy?path=/$CONTEX_NAME&update=true)
 
-CURL_RESPONSE=$(curl -v -u $TOMCAT_USER:$TOMCAT_PASSWORD -T $WAR_PATH http://$TOMCAT_HOST:$TOMCAT_PORT/manager/text/deploy?path=/$CONTEX_NAME&update=true)
+echo \"$CURL_RESPONSE\"
+
 if [[ $CURL_RESPONSE == *\"FAIL\"* ]]; then
 echo \"war deployment failed\"
 exit 1

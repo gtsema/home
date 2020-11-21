0685147ffa4526298ce2e4fde965f487f2dd5147
gtsema <42067282+gtsema@users.noreply.github.com>
2020-11-21 17:31:35 +0300

    Update undeploy.sh

--- jenkinsfiles/undeploy.sh
+++ jenkinsfiles/undeploy.sh
@@ -2,10 +2,10 @@
 
 CURL_RESPONSE=$(curl -u $TOMCAT_USER:$TOMCAT_PASSWORD http://$TOMCAT_HOST:$TOMCAT_PORT/manager/text/undeploy?path=/$CONTEX_NAME)
 
-if [[ $CURL_RESPONSE == *FAIL* ]]; then
-  echo "war deployment failed"
-  exit 1
-else
+if [[ $CURL_RESPONSE == *OK* ]]; then
   echo "war deployed successfully"
   exit 0
+else
+  echo "war deployment failed"
+  exit 1
 fi;

ce8533225034ed55294caecd13e186036db89864
gtsema <42067282+gtsema@users.noreply.github.com>
2020-11-26 19:35:05 +0300

    Update checkDeploying.sh

--- jenkinsfiles/checkDeploying.sh
+++ jenkinsfiles/checkDeploying.sh
@@ -2,7 +2,7 @@
 
 CURL_RESPONSE=$(curl -u $TOMCAT_USER:$TOMCAT_PASSWORD http://$TOMCAT_HOST:$TOMCAT_PORT/manager/text/list)
 
-if [[ $CURL_RESPONSE == *"/:running:0:ROOT"* ]]; then
+if [[ $CURL_RESPONSE =~ .*/:running:[0-9]{1,2}:ROOT.* ]]; then
   echo "deployed"
 else
   echo "undeployed"

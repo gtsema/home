8da4695b8deedf413bcfa0f0858f8d327106bbc4
tsema <g.tsema@gmail.com>
2020-12-17 18:41:35 +0300

    tune...

--- src/main/java/ru/example/home/MvcConfig.java
+++ src/main/java/ru/example/home/MvcConfig.java
@@ -1,23 +1,29 @@
 package ru.example.home;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/js/");
+                .addResourceLocations("/WEB-INF/js/");
 
         registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+                .addResourceLocations("/WEB-INF/static/");
+
+        registry.addResourceHandler("/templates/**")
+                .addResourceLocations("/WEB-INF/templates/");
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/").setViewName("index.html");
     }
 }
